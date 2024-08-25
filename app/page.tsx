c80f3b173fb7c30b7501b3e6377c07e97f7189f2
kazama679 <quanglienha123@gmail.com>
2024-08-25 22:56:42 +0700

    bai9bai10ss8

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+// 'use client'
 import React from 'react'
+import fs from "fs"
+import path from "path"
 
 export default function page() {
+  const filePath=path.join(process.cwd(), 'app/products/product.txt');
+  const data=fs.writeFileSync(filePath, 'hello world11123gggggg', 'utf-8');
+  // console.log('1111111111111111file được đọc với nội dung', data);
+
   return (
     <div>
       123
